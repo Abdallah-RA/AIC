c8df1df9b7a0128369c2151f923bf60305a44167
Abdallah-RA <Abdallah_awad_2003@outlook.com>
2025-06-09 11:46:51 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// next.config.js
-const nextConfig = {
+const basePath = process.env.BASE_PATH || '';
+
+/** @type {import('next').NextConfig} */
+module.exports = {
   output: 'export',
-  basePath: '/AIC',
-  assetPrefix: '/AIC/',
+  basePath,
+  assetPrefix: `${basePath}/`,
   trailingSlash: true,
 }
-
-module.exports = nextConfig
