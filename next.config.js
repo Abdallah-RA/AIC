8e53b5a3cf49b072a56cbb3b366dbd41ccc774cb
Abdallah-RA <Abdallah_awad_2003@outlook.com>
2025-06-09 11:49:44 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const basePath = process.env.BASE_PATH || '';
-
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'export',
-  basePath,
-  assetPrefix: `${basePath}/`,
-  trailingSlash: true,
-}
